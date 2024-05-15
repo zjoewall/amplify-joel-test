1c91233fabca0e7723cfa9b97a273ee3bb01a3af
Joel Wallace <zjoewall@amazon.com>
2024-05-15 13:52:58 -0400

    Revert "backend fix"
    This reverts commit e5e9a94352dd0540fdd9463e674ee776e76fb536.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { storage } from './storage/resource.js';
+import { storage } from './storage/resource';
 
 defineBackend({
   auth,
