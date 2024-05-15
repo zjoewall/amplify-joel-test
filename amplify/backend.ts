9e02f1e9c41a35eaa00418a3871e5eba2edd01c5
Joel Wallace <zjoewall@amazon.com>
2024-05-15 14:17:52 -0400

    why stop

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { storage } from './storage/resource';
+import { storage } from './storage/resource.js';
 
 defineBackend({
   auth,
